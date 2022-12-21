1721bd820c445edb3ebd5d7ae42a66477f2c9c7b
Xevlyn <ewanxev237@aol.com>
2022-12-21 13:01:31 +0100

    edited errors

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "web" {
     ami           = var.ami_id
     instance_type = var.instance_type
+    
+    monitoring = true //this is enabling  monitoring
+    ebs_optismized = true //
+    root_block_device {
+        encrypted  = true //
+    }
 
     tags = {
     Name = "HelloWorld"
